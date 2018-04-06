51928e3cab64383ddd5bf8e08d1099b086ba6c9b
ProgPassion <31067252+ProgPassion@users.noreply.github.com>
2018-04-06 18:59:38 +0200

    Update 2-4.c
    My solution to the exercise, i think is more neater. 
    Anyway I was just checking different solution around the Internet for this. Congrats that you completed all the exercises! :)

--- ch02/2-4.c
+++ ch02/2-4.c
@@ -5,35 +5,25 @@
  */
 
 #include <stdio.h>
-#include <string.h>
 
-#define SIZE 0xff
-
-void squeeze(char s1[], char s2[]);
+void squeezes(char s1[], char s2[]);
 
 int main() {
-  char s1[] = "abcdefabcghi";
-  char s2[] = "aaabc";
-  squeeze(s1, s2);
-  printf("%s\n", s1);
+	
+	char s1[5] = "Imran";
+	char s2[6] = "Blerta";
+	
+	squeezes(s1, s2);
+	printf("%s\n", s1);
+	return 0;
 }
 
-void squeeze(char s1[], char s2[]){
-  char symbol[SIZE] = {};
-  int i, j;
+void squeezes(char s1[], char s2[]) {
+	int i, j, k;
 
-  for (i = 0; s2[i] != '\0'; ++i) {
-    symbol[s2[i]] = 1;
-  }
-
-  i = j = 0;
-
-  while (s1[i] != '\0') {
-    while (symbol[s1[i]]) {
-      i++;
-    }
-    s1[j++] = s1[i++];
-  }
-
-  s1[j] = '\0';
+	for(i = k = 0; s1[i] != '\0'; i++){
+		for(j = 0; s2[j] != '\0' && s1[i] != s2[j]; j++);
+		if(s2[j] == '\0') {s1[k++] = s1[i];}
+	}
+	s1[k] = '\0';
 }
